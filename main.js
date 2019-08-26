d5a9a1ff8982090e2c727302505f08aabb22f163
Leonardo Ramalho de Mendonça <leuramalho.bm@gmail.com>
2019-08-26 18:52:56 +0000

    New

---
+++ main.js
@@ -0,0 +1,19 @@
+function chamarAPI(){
+	console.log("Chamando API");
+	
+	const endpoint = "https://api.exchangeratesapi.io/latest";
+	var oParametros = {
+		base: $("#moeda_origem").val()};
+	$.get(endpoint, oParametros, callback );
+}
+
+function callback(resultado){
+	console.log(resultado);
+	
+	var sMoedaDestino = $("#moeda_destino").val();
+	var fValor		  = resultado.rates[sMoedaDestino];
+	
+	$("#valor_destino").text(fValor);
+	$("#moeda_destino_celula").text(sMoedaDestino);
+	
+}
