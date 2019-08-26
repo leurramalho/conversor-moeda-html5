fa23dc32c9ed7c416923447a9458eefc53d56425
Leonardo Ramalho de Mendonça <leuramalho.bm@gmail.com>
2019-08-26 21:17:40 +0000

    Ajuste

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function chamarAPI(){
 	console.log("Chamando API");
 	
 	const endpoint = "https://api.exchangeratesapi.io/latest";
+	
 	var oParametros = {
 		base: $("#moeda_origem").val()};
 	$.get(endpoint, oParametros, callback );
